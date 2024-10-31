c79c9dcf37c55bf46788997c888fb17d2ed43e34
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2024-10-31 15:54:34 -0300

    feat: create post user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,13 +6,24 @@ const userList = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
   const usuarios = userList.getAllUsers();
-  
+
   return res.status(200).json({
     message:
       usuarios.length == 0
         ? "Não há usuários cadastrados"
         : `Total de usuários: ${usuarios.length}`,
     usuarios,
+  });
+});
+
+usuariosRoutes.post("/", (req, res) => {
+  const { name, email, password} = req.body;
+
+  const user = userList.addUser(name, email, password);
+
+  return res.status(201).json({
+    message: "Usuário cadastrado com sucesso",
+    user,
   });
 });
 
