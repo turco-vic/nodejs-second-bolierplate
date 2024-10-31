1b8faeba45e43c1def02645c5fc6d3680b5d9fc2
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2024-10-31 15:03:35 -0300

    feat: create user class

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,7 +7,7 @@ const routes = Router();
 
 // Rota raiz para teste
 routes.get("/", (req, res) => {
-  return res.status(200).json({ message: "Vai Corinthians!" });
+  return res.status(200).json({ message: "Tudo certo, VAI CORINTHIANS!" });
 });
 
 // Lista de uso das rotas do projeto
